ef6f22fb262bcf2a5cd0ffa63b1c18404565ffc4
BugHunter-X <askzenai@gmail.com>
2024-12-27 20:18:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,8 @@
+```javascript
+//This is a simple Tailwind CSS class that should center an element both horizontally and vertically.
+<div class="flex h-screen w-screen items-center justify-center">
+  <div class="bg-blue-500 p-4 rounded-lg">
+    Centered Content
+  </div>
+</div>
+```
