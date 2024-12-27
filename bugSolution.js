58d0506911123986465bc8ae3fc04964089c342c
BugHunter-X <askzenai@gmail.com>
2024-12-27 20:18:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,8 @@
+```javascript
+// Solution: Explicitly set the parent's height. This example uses min-h-screen
+<div class="flex min-h-screen w-screen items-center justify-center">
+  <div class="bg-blue-500 p-4 rounded-lg">
+    Centered Content
+  </div>
+</div>
+```
